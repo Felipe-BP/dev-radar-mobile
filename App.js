2987a143d4b5e81c8e191c5b4767eea4881e9cc6
Felipe-BP <gvv.fel@gmail.com>
2020-01-25 16:10:41 -0200

    feat: initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7d40e7" />
+      <Routes />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+});
